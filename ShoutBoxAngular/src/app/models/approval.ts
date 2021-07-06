aa46976b126bb70b4ff850bc244e0ea9f56d9036
Rushikesh Phanse <rushikeshp@cybage.com>
2021-07-06 11:14:48 +0530

    angular changes

---
+++ ShoutBoxAngular/src/app/models/approval.ts
@@ -0,0 +1,21 @@
+
+export class Approval {
+    id: string;
+    userName : string;
+    dob : string;
+    mobileNo :  string;
+    userEmail : string;
+    createdAt:any;
+    updatedAt:any;
+
+
+constructor(){
+    this.id = "";
+    this.userName = "";
+    this.dob = "";
+    this.mobileNo = "";
+    this.userEmail = "";
+    this.createdAt = null;
+    this.updatedAt = null;
+}
+}
