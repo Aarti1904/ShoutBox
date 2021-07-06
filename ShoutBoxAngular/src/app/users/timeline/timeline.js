6802ee3259b5b244aeae4eec99ae984b7dec2618
Rushikesh Phanse <82897909+rushikeshph@users.noreply.github.com>
2021-07-06 10:46:46 +0530

    Delete ShoutBoxAngular directory

--- ShoutBoxAngular/src/app/users/timeline/timeline.js
+++
@@ -1,7 +0,0 @@
-/* let selectedUsers=[{name= "Lamborgini",album= "Lamborgini",category="Rock"},
-{name= "Dynamite",album= "BTS",category="Rock"}];
-
-$("#search").keyup(function(){
-   alert("key press");
-
-}); */
